ebce3c71778be7b5dbd67f8960463272071619b9
Sascha Rechenberger <sascha.rechenberger@uni-ulm.de>
2017-07-10 19:19:47 +0200

    first nearly works

--- parser.lisp
+++ parser.lisp
@@ -1,24 +1,66 @@
 (in-package :de.srechenberger.parser)
 
 (defparameter *test-grammar*
-  '((A (B C))
-    (B (C))
-    (C :c)))
+  (list (list 'A (list 'B 'C))
+	(list 'A (list :d :e))
+	(list 'B (list 'C))
+	(list 'C (list :c))
+	(list 'B (list))))
 
-(defvar *first-sets* nil)
-(defvar *non-terminals* nil)
-(defvar *grammar* nil)
+(defparameter *first-sets* (make-hash-table))
+(defparameter *non-terminals* nil)
+(defparameter *grammar* nil)
 
-(defun first-set (alpha visited)
+(defmacro unions (&body sets)
+  `(reduce #'union (list ,@sets)))
+
+(defun non-term-p (symb)
+  (member symb *non-terminals*))
+
+(defun term-p (symb)
+  (and (not (listp symb))
+       (not (non-term-p symb))))
+
+(defun first-set (symb)
   (cond
-    ((not (find alpha *non-terminals*))
-     alpha)
-    ((not (find alpha visited))
-     (push alpha visited)
-     (nconc (mapcar #'(lambda (bodysym)
-			(first-set bodysym visited))
-		    (mapcar #'cdr
-			    (remove-if-not #'(lambda (rule)
-					       (equal (car rule) alpha))
-					   *grammar*))))
-    (t (list))))
+    ((term-p symb)
+     (format t "I ~s~%" symb)
+     (list symb))
+    ((non-term-p symb) 
+     (format t "II ~s~%" symb)
+     (let ((found (gethash symb *first-sets*)))
+       (if found found
+	   (let ((calculated
+		  (reduce #'union
+			  (loop
+			     for (head body) in *grammar*
+			     when (equal head symb)
+			     collect (first-set body)))))
+	     (setf (gethash symb *first-sets*) calculated)))))
+    ((and (listp symb) (not symb))
+     (format t "III ~s~%" symb)
+     (list :eps))
+    ((listp symb)
+     (format t "IV ~s~%" symb)
+     (loop
+	for h in symb
+	with f = (first-set h)
+	collect f into bucket
+	while (not (member :eps f))
+	finally (return (if (member :eps f)
+			    (reduce #'union bucket)
+			    (remove :eps (reduce #'union bucket))))))))
+
+(defmacro with-grammar (grammar &body body)
+  `(let ((*grammar* ,grammar)
+	 (*non-terminals* (mapcar #'first ,grammar)))
+     ,@body))
+		  
+		
+	     
+  
+			    
+			  
+	       
+		    
+  
